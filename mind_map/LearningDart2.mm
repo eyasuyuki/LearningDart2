95b953526b05899533ed7d79c8a95cd0b03e9fb5
ENDO Yasuyuki <eyasuyuki@gmail.com>
2019-06-17 21:09:19 +0900

    initial commit

---
+++ mind_map/LearningDart2.mm
@@ -0,0 +1,113 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Flutter&#x3068;AngularDart&#x3067;Dart2&#x5165;&#x9580;(&#x4eee;)" FOLDED="false" ID="ID_1938364472" CREATED="1560764075413" MODIFIED="1560769595562" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Dart2&#x306e;&#x3069;&#x3053;&#x304c;&#x826f;&#x3044;&#x306e;&#x304b;?" POSITION="left" ID="ID_664552663" CREATED="1560767810501" MODIFIED="1560769520346">
+<edge COLOR="#00ffff"/>
+<node TEXT="&#x8a2d;&#x8a08;&#x601d;&#x60f3;" ID="ID_603614963" CREATED="1560768177522" MODIFIED="1560768183549"/>
+<node TEXT="&#x8a00;&#x8a9e;&#x4ed5;&#x69d8;" ID="ID_1272182206" CREATED="1560767892027" MODIFIED="1560767898367"/>
+<node TEXT="&#x30a8;&#x30b3;&#x30b7;&#x30b9;&#x30c6;&#x30e0;" ID="ID_44173282" CREATED="1560767880709" MODIFIED="1560767887919"/>
+<node TEXT="&#x4ed6;&#x306e;&#x8a00;&#x8a9e;&#x3068;&#x306e;&#x6bd4;&#x8f03;" ID="ID_1798336904" CREATED="1560767825381" MODIFIED="1560767833455">
+<node TEXT="JavaScript" ID="ID_245834360" CREATED="1560767834265" MODIFIED="1560767842959"/>
+<node TEXT="TypeScript" ID="ID_1246037985" CREATED="1560767845803" MODIFIED="1560767848639"/>
+</node>
+<node TEXT="&#x30e2;&#x30d0;&#x30a4;&#x30eb;&#x3068;&#x30d5;&#x30ed;&#x30f3;&#x30c8;&#x30a8;&#x30f3;&#x30c9;&#x3067;&#x8a2d;&#x8a08;&#x3092;&#x5171;&#x901a;&#x5316;" ID="ID_922380388" CREATED="1560769262946" MODIFIED="1560769277331"/>
+</node>
+<node TEXT="&#x3069;&#x3046;&#x3084;&#x3063;&#x3066;&#x5165;&#x9580;&#x3059;&#x308b;&#x306e;&#x304c;&#x826f;&#x3044;&#x306e;&#x304b;?" POSITION="right" ID="ID_839619641" CREATED="1560767947679" MODIFIED="1560767960485">
+<edge COLOR="#7c0000"/>
+<node TEXT="&#x60c5;&#x5831;&#x6e90;&#x306f;?" ID="ID_733595041" CREATED="1560767972957" MODIFIED="1560767985654">
+<node TEXT="&#x516c;&#x5f0f;&#x30c9;&#x30ad;&#x30e5;&#x30e1;&#x30f3;&#x30c8;" ID="ID_288387656" CREATED="1560768284143" MODIFIED="1560768291131">
+<node TEXT="Language Tour" ID="ID_810874248" CREATED="1560767986227" MODIFIED="1560767991908"/>
+<node TEXT="Effective Dart" ID="ID_528444101" CREATED="1560767993875" MODIFIED="1560767998181"/>
+</node>
+<node TEXT="&#x30b3;&#x30df;&#x30e5;&#x30cb;&#x30c6;&#x30a3;" ID="ID_111538329" CREATED="1560768292973" MODIFIED="1560768299089"/>
+<node TEXT="&#x66f8;&#x7c4d;?" ID="ID_191126173" CREATED="1560768326987" MODIFIED="1560768332389"/>
+</node>
+<node TEXT="&#x74b0;&#x5883;&#x69cb;&#x7bc9;&#x306f;?" ID="ID_458551744" CREATED="1560768227238" MODIFIED="1560768237545">
+<node TEXT="&#x304a;&#x3059;&#x3059;&#x3081;&#x306e;&#x30a8;&#x30c7;&#x30a3;&#x30bf;&#x306f;?" ID="ID_1792480089" CREATED="1560768238031" MODIFIED="1560768253583"/>
+</node>
+</node>
+<node TEXT="Dart2&#x5165;&#x9580;&#x306e;&#x96e3;&#x95a2;&#x306f;&#x4f55;&#x304b;?" POSITION="left" ID="ID_1096383926" CREATED="1560768004144" MODIFIED="1560769526184">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="&#x306a;&#x305c;Dart2&#x5165;&#x9580;&#x306a;&#x306e;&#x304b;?" POSITION="right" ID="ID_588492477" CREATED="1560768456114" MODIFIED="1560768544176">
+<edge COLOR="#007c00"/>
+<node TEXT="Flutter&#x3092;&#x3084;&#x3063;&#x3066;&#x77e5;&#x308b;Dart&#x306e;&#x826f;&#x3055;" ID="ID_1052778679" CREATED="1560764275084" MODIFIED="1560768551925"/>
+<node TEXT="Web&#x30d5;&#x30ed;&#x30f3;&#x30c8;&#x30a8;&#x30f3;&#x30c9;&#x3067;&#x306e;&#x518d;&#x8a55;&#x4fa1;" ID="ID_1258499084" CREATED="1560767644460" MODIFIED="1560768562365">
+<node TEXT="AngularDart" ID="ID_1851166904" CREATED="1560767685083" MODIFIED="1560767693323"/>
+</node>
+<node TEXT="Dart1&#x306e;&#x53e4;&#x3044;&#x60c5;&#x5831;&#x304c;&#x591a;&#x3044;" ID="ID_1005853467" CREATED="1560764162388" MODIFIED="1560768559280">
+<node TEXT="Dart1&#x3068;2&#x306e;&#x9055;&#x3044;&#x3092;&#x7406;&#x89e3;&#x3057;&#x3066;&#x3082;&#x3089;&#x3046;" ID="ID_44149599" CREATED="1560764242115" MODIFIED="1560764258347"/>
+</node>
+</node>
+<node TEXT="Dart&#x306f;&#x3053;&#x308c;&#x304b;&#x3089;&#x3069;&#x3046;&#x306a;&#x308b;?" POSITION="right" ID="ID_689523672" CREATED="1560769191104" MODIFIED="1560769198857">
+<edge COLOR="#7c007c"/>
+<node TEXT="Flutter Web" ID="ID_1359214953" CREATED="1560769199567" MODIFIED="1560769204330"/>
+<node TEXT="Flutter Desktop Embedded" ID="ID_174041583" CREATED="1560769209635" MODIFIED="1560769301927"/>
+<node TEXT="Fuchsia" ID="ID_1556064922" CREATED="1560769306212" MODIFIED="1560769361931"/>
+</node>
+</node>
+</map>
